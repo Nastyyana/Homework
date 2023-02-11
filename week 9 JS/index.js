cb7478b0ad5c255d0ab316998ea37665de4d78d5
Anastasiia Vorobeva <nastya.eliseevaa7@gmail.com>
2023-02-10 22:35:31 -0600

    Add JS exercises for week 9

---
+++ week 9 JS/index.js
@@ -0,0 +1,37 @@
+// 1
+console.log(40000 + 22000);
+
+// 2
+let word = "Владивосток ";
+let n = 2000;
+console.log(word + n);
+
+// 3
+console.log(128 + 254 + 137 +201);
+
+// 4
+console.log(2 ** 5);
+
+// 5
+console.log("Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!");
+
+// 6
+const greetings = "Привет";
+const name = "Кот";
+console.log(greetings + ", " + name);
+
+// 7
+let time;
+time = 34;
+console.log('Старт поездки. Осталось минут: ' + time);
+time -= 15;
+console.log('Старт поездки. Осталось минут: ' + time);
+time -= 10;
+console.log('Немного не новостей, но событий. Осталось минут: ' + time);
+time = 0;
+console.log('Вы приехали. Добро пожаловать в Москву');
+
+// 8
+let fahrenheit = 451;
+let celsius = ((fahrenheit - 32) / 1.8);
+console.log(`${fahrenheit} градуса по Фаренгейту — это ${celsius} градуса по Цельсию.`)
